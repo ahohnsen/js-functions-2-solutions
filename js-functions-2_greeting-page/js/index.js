437c7a5a601dab7562b04caf329581c3a7f5a0be
Andrea Hohnsen <a.hohnsen@mail.de>
2024-04-25 14:34:51 +0200

    Initial commit

---
+++ js-functions-2_greeting-page/js/index.js
@@ -0,0 +1,62 @@
+console.clear();
+
+/*
+Change the contents of this page depending on the current day and time.
+
+- Write a function getGreeting that returns a different greeting depending on the current time:
+  - 6 - 12: returns "Good Morning"
+  - 13 - 18: returns "Good Afternoon"
+  - 19 - 22: returns "Good Evening"
+  - 23 - 5: returns "Good Night"
+
+(HINT: you can get the current time: new Date().getHours() )
+
+- Write a function getDayColor that returns a different color depending on the current weekday:
+  - monday: "darkgray"
+  - tuesday - friday: "lightblue"
+  - saturday - sunday: "hotpink"
+
+(HINT: you can get the current weekday: new Date().getDay() )
+
+*/
+
+const display = document.querySelector('[data-js="display"]');
+
+function getGreeting() {
+  const currentTime = new Date().getHours();
+  if (currentTime >= 6 && currentTime <= 12) {
+    return "Good Morning";
+  } else if (currentTime >= 13 && currentTime <= 18) {
+    return "Good Afternoon";
+  } else if (currentTime >= 19 && currentTime <= 22) {
+    return "Good Evening";
+  } else {
+    return "Good Night";
+  }
+}
+
+// function getGreeting() {
+//   const currentTime = new Date().getHours();
+//   if (currentTime >= 6 && currentTime <= 12) return "Good Morning";
+//   if (currentTime >= 13 && currentTime <= 18) return "Good Afternoon";
+//   if (currentTime >= 19 && currentTime <= 22) return "Good Evening";
+//   return "Good Night";
+// }
+
+// function getGreeting() {
+//   const currentHour = new Date().getHours();
+//   if (currentHour < 6) return "Good Night";
+//   if (currentHour < 13) return "Good Morning";
+//   if (currentHour < 19) return "Good Afternoon";
+//   return "Good Evening";
+// }
+
+function getDayColor() {
+  const currentWeekday = new Date().getDay();
+  if (currentWeekday === 1) return "darkgray";
+  if (currentWeekday === 6 || currentWeekday === 0) return "hotpink";
+  return "lightblue";
+}
+
+display.textContent = getGreeting();
+document.body.style.backgroundColor = getDayColor();
